0a4544a8aec6f24205920203686bbe6987820b55
suleimin <suleimin@gmail.com>
2025-01-23 16:57:19 +0900

    initial commit

---
+++ src/main/java/com/example/serachBook/Domain/Book.java
@@ -0,0 +1,99 @@
+package com.example.serachBook.Domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+
+	private String title;
+
+
+	private String author;
+
+
+
+	private int year;
+
+
+	private String genre;
+
+	// Default constructor
+	public Book() {
+	}
+
+	public Book(int id,String title, String author, int year, String genre) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.genre = genre;
+	}
+
+
+	// Parameterized constructor
+	public Book(String title, String author, int year, String genre) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.genre = genre;
+	}
+
+	// Getters and Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	// Override toString method
+	@Override
+	public String toString() {
+		return "Book{" +
+				"id=" + id +
+				", title='" + title + '\'' +
+				", author='" + author + '\'' +
+				", year=" + year +
+				", genre='" + genre + '\'' +
+				'}';
+	}
+}
---
+++ src/main/java/com/example/serachBook/Service/BookService.java
@@ -0,0 +1,45 @@
+package com.example.serachBook.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.serachBook.Domain.Book;
+import com.example.serachBook.Repository.BookRepository;
+
+@Service
+public class BookService {
+
+	 @Autowired
+	 private BookRepository bookRepository;
+	 
+	 public List<Book> listBookAll(){
+		 return bookRepository.findAll();
+	 }
+	 
+	 public List<Book> listAll(String keyword){
+		 return bookRepository.findByTitleContainingIgnoreCase(keyword);
+	 }
+	 
+	 public Book getBookById(int id) {
+		    Optional<Book> userOptional = bookRepository.findById((long) id);
+		    if(userOptional.isPresent()) {
+		        return userOptional.get();
+		    }
+		    return null; // or throw a custom exception
+		}
+	 
+	  public void updateBook(Book book) {
+	        bookRepository.save(book); // Save or update the book
+	    }
+	 
+	  public void deleteBookById(long id) {
+	        if (bookRepository.existsById(id)) {
+	            bookRepository.deleteById(id); // Delete the book if it exists
+	        } else {
+	            throw new IllegalArgumentException("Book with ID " + id + " not found");
+	        }
+	    }
+}
