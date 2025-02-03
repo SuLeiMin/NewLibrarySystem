8fd7b743fada13f71523e45bd3db34c09dadfe85
SuLeiMin <100254264+SuLeiMin@users.noreply.github.com>
2025-02-03 15:57:57 +0900

    Update SerachBookApplication.java

--- src/main/java/com/example/librarysystem/SerachBookApplication.java
+++ src/main/java/com/example/librarysystem/SerachBookApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SerachBookApplication {
+public class LibrarySystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SerachBookApplication.class, args);
